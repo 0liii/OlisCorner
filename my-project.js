78eed90e7524284af17b4811a054b1510be17cf2
0liii <olivia.wyzga@gmail.com>
2022-07-26 16:47:47 -0700

    added a contact form & started incorporating some javascript! :)

---
+++ my-project.js
@@ -0,0 +1,13 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+ fields.firstName = document.getElementById('fname');
+ fields.lastName = document.getElementById('lname');
+ fields.subject = document.getElementById('subject');
+ fields.email = document.getElementById('email');
+ fields.message = document.getElementById('message');
+})
+
+function submit() {
+
+}
